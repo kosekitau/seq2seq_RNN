30d48c0a8cc20e99cf63d5548e53a2c002376403
koseki <kosekitau@gmail.com>
2020-05-14 09:39:39 +0900

    make_txt.pyに文章の長さの指定してデータを抽出を行うように変更した。

--- japanese/make_txt.py
+++ japanese/make_txt.py
@@ -1,6 +1,6 @@
 from os import listdir, path
 import json
-
+import collections
 #対話破綻コーパスから入力応答ペアのテキストデータを生成する
 
 
@@ -28,13 +28,29 @@ def corpus_to_sentences(corpus):
 corpus = corpus_files()#jsonファイルへのパスリストを生成
 sentences = corpus_to_sentences(corpus)#一文ずつリストに保存
 data = []
+length = []
 
 for i in range(len(sentences)):
   for j in range(len(sentences[i]["turns"])-1):
     #入力と応答でタブ区切り
+    length.append(len(sentences[i]["turns"][j]["utterance"]))
     data.append(sentences[i]["turns"][j]["utterance"] + '\t' + sentences[i]["turns"][j+1]["utterance"])
 
 print('length: ', len(data))
+c = collections.Counter(length)
+
+MAX_LENGTH = 36 #最大単語数(句読点を含む)
+
+#上記2つの条件を満たすデータのみを抽出
+def filterPair(p):
+    return len(p.split('\t')[0]) < MAX_LENGTH and \
+        len(p.split('\t')[1]) < MAX_LENGTH
+
+def filterPairs(pairs):
+    return [pair for pair in pairs if filterPair(pair)]
+    #return [pair for pair in pairs if filterPair(pair)]
+data = filterPairs(data)
+print('filter length', len(data))
 #テキストファイルに出力
 with open('unk.txt', 'w') as f:
     f.write('\n'.join(data))
